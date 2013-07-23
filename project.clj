ae18f72aa0e7bf8822ee73231af578292f75a4d1
supersymmetry <maildumpert@gmail.com>
2013-07-23 05:56:11 +0200

    update refs

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject org.clojars.owl/clojure.otp "0.2-BETA"
   :description "OTP-like System for clojure"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
 		 [org.clojure/clojure-contrib "1.2.0"]
-		 [pattern-match "1.0.0"]])
+		 [pattern-match "1.0.0"]]
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"})
